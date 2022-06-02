4f0ff2d623ba5726b216ae7e9aeabd937214943a
Technologie31 <78386526+Technologie31@users.noreply.github.com>
2022-06-02 13:50:05 +0200

    Mise à jour pxt.json, main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,10 +1,10 @@
-basic.showIcon(IconNames.Heart)
+basic.showIcon(IconNames.Yes)
 basic.forever(function () {
     if (pins.digitalReadPin(DigitalPin.P13) == 1 || (pins.digitalReadPin(DigitalPin.P0) == 0 || pins.digitalReadPin(DigitalPin.P1) == 0)) {
         pins.digitalWritePin(DigitalPin.P14, 1)
-        servos.P2.setAngle(90)
+        servos.P2.setAngle(0)
     } else {
         pins.digitalWritePin(DigitalPin.P14, 0)
-        servos.P2.setAngle(0)
+        servos.P2.setAngle(90)
     }
 })
