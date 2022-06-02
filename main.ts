6563c067a674fb53fbec01bfc69f8642e750b408
Technologie31 <78386526+Technologie31@users.noreply.github.com>
2022-06-02 13:41:17 +0200

    Fichiers initiaux pour un projet MakeCode

---
+++ main.ts
@@ -0,0 +1,10 @@
+basic.showIcon(IconNames.Heart)
+basic.forever(function () {
+    if (pins.digitalReadPin(DigitalPin.P13) == 1 || (pins.digitalReadPin(DigitalPin.P0) == 0 || pins.digitalReadPin(DigitalPin.P1) == 0)) {
+        pins.digitalWritePin(DigitalPin.P14, 1)
+        servos.P2.setAngle(90)
+    } else {
+        pins.digitalWritePin(DigitalPin.P14, 0)
+        servos.P2.setAngle(0)
+    }
+})
